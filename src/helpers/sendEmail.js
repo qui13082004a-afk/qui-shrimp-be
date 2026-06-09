f3c47b9b4d1453dd6701c90f45a4262f5691ceaf
Nguyễn Văn Quí <qui13082004a@gmail.com>
2026-06-09 18:44:23 +0700

    sửa sendmail

--- src/helpers/sendEmail.js
+++ src/helpers/sendEmail.js
@@ -6,6 +6,9 @@ const sendEmail = async (to, subject, text) => {
     port: 465,
     secure: true,
     family: 4,
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,

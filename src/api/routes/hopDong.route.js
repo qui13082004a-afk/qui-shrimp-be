de734004c496fa2a03559da39dac89179adf0a3a
Nguyễn Văn Quí <qui13082004a@gmail.com>
2026-07-08 13:26:40 +0700

    fix hop dong

--- src/api/routes/hopDong.route.js
+++ src/api/routes/hopDong.route.js
@@ -4,57 +4,59 @@ const router = express.Router();
 const hopDongController = require("../controllers/hopDong.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
 const { authorizeAdmin } = authMiddleware;
+const upload = require("../middlewares/upload.middleware");
 
 router.post(
-    "/",
-    authMiddleware,
-    authorizeAdmin,
-    hopDongController.createContract
+  "/",
+  authMiddleware,
+  authorizeAdmin,
+  hopDongController.createContract
 );
 
 router.get(
-    "/admin",
-    authMiddleware,
-    authorizeAdmin,
-    hopDongController.getAllContracts
+  "/admin",
+  authMiddleware,
+  authorizeAdmin,
+  hopDongController.getAllContracts
 );
 
 router.get(
-    "/my",
-    authMiddleware,
-    hopDongController.getMyContracts
+  "/my",
+  authMiddleware,
+  hopDongController.getMyContracts
 );
 
 router.get(
-    "/profile/:profileId",
-    authMiddleware,
-    hopDongController.getContractByProfileId
+  "/profile/:profileId",
+  authMiddleware,
+  hopDongController.getContractByProfileId
 );
 
 router.put(
-    "/:id/upload",
-    authMiddleware,
-    hopDongController.uploadSignedContract
+  "/:id/upload",
+  authMiddleware,
+  upload.single("file_hop_dong"),
+  hopDongController.uploadSignedContract
 );
 
 router.put(
-    "/:id/confirm",
-    authMiddleware,
-    authorizeAdmin,
-    hopDongController.confirmContract
+  "/:id/confirm",
+  authMiddleware,
+  authorizeAdmin,
+  hopDongController.confirmContract
 );
 
 router.put(
-    "/:id/cancel",
-    authMiddleware,
-    authorizeAdmin,
-    hopDongController.cancelContract
+  "/:id/cancel",
+  authMiddleware,
+  authorizeAdmin,
+  hopDongController.cancelContract
 );
 
 router.get(
-    "/:id",
-    authMiddleware,
-    hopDongController.getContractById
+  "/:id",
+  authMiddleware,
+  hopDongController.getContractById
 );
 
 module.exports = router;
